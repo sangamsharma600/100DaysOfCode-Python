f87bd40479fee3f64ecc3de63a00a3e9a9dcb0d0
Sangam Sharma <sangamsharma600@gmail.com>
2021-11-18 18:52:56 +0545

    Day1

---
+++ Day 1.py
@@ -0,0 +1,8 @@
+# /*Resturant Name Generator😁😁😁*\ #
+#  "This is just for fun. Don't use the generated name 😂."
+print("Welcome to children's name Generator.......")
+fName=input("Enter your favourite food's name : ")
+pName=input("Enter your favourite place name : ")
+print("Your resturant name could be : "+pName+" "+fName)
+
+#END OF CODE
