7a65be9d30aeb69f9b7579eaf2951293aa45f71e
Sangam Sharma <sangamsharma600@gmail.com>
2021-11-27 23:03:27 +0545

    Days

--- Day 8/Day 8b.py
+++ Day 8/Day 8b.py
@@ -4,38 +4,38 @@ alphabets=[
     'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
 ]
 
+is_continue=True
 
-enc_dec=input("What would you like to do ? Encrypt or Decrypt :").lower()
-if enc_dec=='encrypt' or enc_dec=='decrypt':
-    shifts=int(input("How much shift do you want ?"))
-    text=input("Enter your text: ").lower()
-
-def encrypt(text,shifts):
-    encrypted_text=''
-    for letter in text:
-        old_position=alphabets.index(letter)
-        new_position=old_position+shifts
-        encrypted_text +=alphabets[new_position]
-    print(f"Your encrypted text is {encrypted_text}")
+while is_continue:
+    enc_dec=input("What would you like to do ? Encrypt or Decrypt :").lower()
+    if enc_dec=='encrypt' or enc_dec=='decrypt':
+        shifts=int(input("How much shift do you want ?"))
+        shifts%=26
+        text=input("Enter your text: ").lower()
 
 
-def decrypt(text,shifts):
-    decrypted_text=''
-    for letter in text:
-        old_position=alphabets.index(letter)    
-        new_position=old_position-shifts
-        decrypted_text+=alphabets[new_position]
-    print(f"Your decrypted text is {decrypted_text}")
-
-
-# encrypt(text=text,shifts=shifts)
-
-if enc_dec=='decrypt':
-    decrypt(text,shifts)
-elif enc_dec=='encrypt':
-    encrypt(text,shifts)
-else:
-    print('Please only enter either encrypt or decrypt 🙂')
+        def caesar(text,shifts,enc_dec):
+            final_result=''
+            for letter in text:
+                if letter in alphabets:
+                    old_position=alphabets.index(letter)
+                    if enc_dec=='encrypt':
+                        new_position=old_position+shifts
+                        final_result+=alphabets[new_position]
+                    elif enc_dec=='decrypt':
+                        new_position=old_position-shifts
+                        final_result+=alphabets[new_position]
+                else:
+                    final_result+=letter
+            print(f'Your {enc_dec}ed data is {final_result}')
+    
+        caesar(text,shifts,enc_dec)
+        result=input('Do you like to use the program again ? Yes or No : ')
+        if result=='no':
+            is_continue=False
+            print("Thank You for using.\n Good Luck")
+    else:
+        print("Please enter a valid input")
 
     #ENDOFCODE#
 
