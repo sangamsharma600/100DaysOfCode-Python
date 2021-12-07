d7a90d832beb8be0906addfaa86385fb8f34a333
Sangam Sharma <sangamsharma600@gmail.com>
2021-12-07 23:39:20 +0545

    Days

---
+++ Day 16/Day 16a.py
@@ -0,0 +1,15 @@
+# /*TAPPING INTO TURTLE GRAPHICS*\
+from turtle import Turtle,Screen
+my_turtle=Turtle()
+my_turtle.color('red','yellow')
+my_turtle.forward(100)
+my_turtle.backward(200)
+# my_turtle.up(100)
+
+print(my_turtle.shape('turtle'))
+
+
+my_screen=Screen()
+print(my_screen.canvheight)
+# my_screen.exitonclick()
+
