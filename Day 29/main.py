259f30a7931bc96182d84beea89d2df3e5b52d24
Sangam Sharma <sangamsharma600@gmail.com>
2022-02-01 23:18:08 +0545

    Day 29

--- Day 29/main.py
+++ Day 29/main.py
@@ -1,34 +1,31 @@
-# import smtplib
-
-
-# TODO: Use your own email and password #
-my_email = "myemail@gmail.com"
-password = "Password"
-# # Securing Email Privacy #
-# with smtplib.SMTP("smtp.gmail.com", 587) as connection:
-#     connection.starttls()
-#     connection.login(user=my_email, password=password)
-#     connection.sendmail(from_addr=my_email, to_addrs="sangamsharma600@gmail.com", msg="Subject:Hello There !!!\n\nHello there, this is a testing mail.")
-
-
-
-
-# import datetime as dt
-
 import datetime as dt
 import random
 import smtplib
+from email.mime.text import MIMEText
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+my_email = os.getenv('my_email')
+password =os.getenv('password')
 
 now = dt.datetime.now()
 weekday = now.weekday()
 
 if weekday == 6:
-    with open("Day 29/quotes.txt", encoding="utf-8") as quotes_file:
+     with open("Day 29/quotes.txt", encoding="utf-8") as quotes_file:
         all_quotes = quotes_file.readlines()
         quote = random.choice(all_quotes)
-        print(quote)
+        data=quote.encode(encoding='utf-8')
+        decode_data=data.decode('utf-8')
+        msg = MIMEText(decode_data)
+        msg['Subject'] = 'Happy Sunday'
+        msg['From'] = my_email
+        receivers='receiversemail@gmail.com'
+        
+        with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+            connection.starttls()
+            connection.login(my_email, password)
+            connection.sendmail(my_email, receivers, msg.as_string())
     
-    with smtplib.SMTP("smtp.gmail.com", 465) as connection:
-        connection.starttls()
-        connection.login(my_email, password)
-        connection.sendmail(from_addr=my_email, to_addrs='sangamsharma600@gmail.com', msg=f"Subject:Happy Sunday\n\n{quote.encode()}")
